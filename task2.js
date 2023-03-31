e17a62ccff516abb5690b45afd075c9144814cf9
SeoA <leeej212@naver.com>
2023-03-31 20:40:34 +0900

    과제 끝 !

--- task2.js
+++ task2.js
@@ -1,1 +1,13 @@
 /* 2번 문제 답안을 작성해주세요. */
+function solution(num_list) {
+  let x = 0;
+  let y = 0;
+  let answer = [];
+
+  for (let i = 0; i < num_list.length; i++) {
+    if (num_list[i] % 2 === 0) x = x + 1;
+    else y = y + 1;
+  }
+  answer = [x, y];
+  return answer;
+}
--- task3.js
+++ task3.js
@@ -1,1 +1,11 @@
 /* 3번 문제 답안을 작성해주세요. */
+function solution(n) {
+  let x = 1;
+
+  for (let i = 1; i <= 10; i++) {
+    x *= i;
+    if (x > n) {
+      return i - 1;
+    }
+  }
+}
